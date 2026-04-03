682049616fc2130ebeadc1cae18b53bb93a502d0
Liam_laptop <throwaway24259@gmail.com>
2026-04-03 13:54:33 +0700

    feat: initialize hospital database schema - created core tables (department, staff, patient, medical_record, service_record, prescription) - defined primary keys and foreign key relationships - added constraints (gender, staff_role) - configured hospital_dba user and permissions

---
+++ QuanLyYTe/Script/Setup/CreateDBA_QuanLyYTe.sql
@@ -0,0 +1,26 @@
+-- Sử dụng database XEPDB1
+ALTER SESSION SET CONTAINER = xepdb1;
+
+--  Tạo User Admin cho đồ án và cấp các quyền cần thiết để DBA có thể sử dụng database
+CREATE USER hospital_dba IDENTIFIED BY 123;
+
+GRANT connect, resource, dba TO hospital_dba;
+
+GRANT
+    UNLIMITED TABLESPACE
+TO hospital_dba;
+
+-- Cấp quyền để HOSPITAL_DBA có thể quản lý User cho Phân hệ 1
+GRANT
+    CREATE USER,
+    ALTER USER,
+    DROP USER
+TO hospital_dba;
+
+GRANT
+    GRANT ANY ROLE
+TO hospital_dba;
+
+-- Delete user
+-- Must log in as SYSDBA to execute
+-- DROP USER hospital_dba CASCADE;
