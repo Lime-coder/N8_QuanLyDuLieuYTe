ed6f6fe0f86782a5916a0714673fb6310e81a6c3
Liam_laptop <throwaway24259@gmail.com>
2026-06-23 15:49:24 +0700

    fix incorrect labeling for OLS

--- QuanLyYTe/Script/database/06_ols/05_ols_tests.sql
+++ QuanLyYTe/Script/database/06_ols/05_ols_tests.sql
@@ -21,7 +21,7 @@ BEGIN
     VALUES ('NV_U3', UNISTR('L\00E3nh \0111\1EA1o Th\1EA7n kinh'), 'Nam', TO_DATE('1981-01-01','YYYY-MM-DD'), 'OLS_003', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB02', UNISTR('B\00E1c s\0129'), 'U3', UNISTR('H\00E0 N\1ED9i'));
 
     INSERT INTO hospital.staff (staff_id, full_name, gender, birthdate, id_card, hometown, phone, dept_id, staff_role, username_db, facility)
-    VALUES ('NV_U4', UNISTR('Nh\00E2n vi\00EAn Th\1EA7n kinh'), UNISTR('N\1EEF'), TO_DATE('1990-01-01','YYYY-MM-DD'), 'OLS_004', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB02', UNISTR('K\1EF9 thu\1EADt vi\00EAn'), 'U4', UNISTR('H\00E0 N\1ED9i'));
+    VALUES ('NV_U4', UNISTR('Nh\00E2n vi\00EAn Th\1EA7n kinh'), UNISTR('N\1EEF'), TO_DATE('1990-01-01','YYYY-MM-DD'), 'OLS_004', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB02', UNISTR('K\1EF9 thu\1EADt vi\00EAn'), 'U4', UNISTR('H\1ED3 Ch\00ED Minh'));
 
     INSERT INTO hospital.staff (staff_id, full_name, gender, birthdate, id_card, hometown, phone, dept_id, staff_role, username_db, facility)
     VALUES ('NV_U5', UNISTR('Nh\00E2n vi\00EAn Tim m\1EA1ch'), 'Nam', TO_DATE('1992-01-01','YYYY-MM-DD'), 'OLS_005', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB03', UNISTR('K\1EF9 thu\1EADt vi\00EAn'), 'U5', UNISTR('H\1ED3 Ch\00ED Minh'));
@@ -33,7 +33,7 @@ BEGIN
     VALUES ('NV_U7', UNISTR('L\00E3nh \0111\1EA1o ph\00F2ng T\1ED5ng h\1EE3p'), 'Nam', TO_DATE('1984-01-01','YYYY-MM-DD'), 'OLS_007', UNISTR('Ch\01B0a r\00F5'), '0000000000', NULL, UNISTR('B\00E1c s\0129'), 'U7', UNISTR('H\1EA3i Ph\00F2ng'));
 
     INSERT INTO hospital.staff (staff_id, full_name, gender, birthdate, id_card, hometown, phone, dept_id, staff_role, username_db, facility)
-    VALUES ('NV_U8', UNISTR('Nh\00E2n vi\00EAn Ti\00EAu h\00F3a'), UNISTR('N\1EEF'), TO_DATE('1995-01-01','YYYY-MM-DD'), 'OLS_008', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB01', UNISTR('K\1EF9 thu\1EADt vi\00EAn'), 'U8', UNISTR('H\1EA3i Ph\00F2ng'));
+    VALUES ('NV_U8', UNISTR('Nh\00E2n vi\00EAn Ti\00EAu h\00F3a'), UNISTR('N\1EEF'), TO_DATE('1995-01-01','YYYY-MM-DD'), 'OLS_008', UNISTR('Ch\01B0a r\00F5'), '0000000000', 'PB01', UNISTR('K\1EF9 thu\1EADt vi\00EAn'), 'U8', UNISTR('H\00E0 N\1ED9i'));
 
     COMMIT;
 END;
@@ -75,10 +75,10 @@ BEGIN
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U1', 'BGD:TH,TK,TM:HCM,HP,HN');
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U2', 'LDK:TM:HCM');
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U3', 'LDK:TK:HN');
-    SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U4', 'NV:TK:HN');
+    SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U4', 'NV:TK:HCM');
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U5', 'NV:TM:HCM');
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U6', 'LDK:TM:HCM');
     SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U7', 'LDK:TH,TK,TM:HCM,HP,HN');
-    SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U8', 'NV:TH:HP');
+    SA_USER_ADMIN.SET_USER_LABELS('HOSP_OLS_POL', 'U8', 'NV:TH:HN');
 END;
 /
